ab2eda3f094d2abd5a3e8cbf9cc956dac326a170
Amritpal Singh <amritpalsingh@Mac.galaxus.box>
2025-12-15 21:41:04 +0100

    First Commit

---
+++ src/main/java/com/aps/docustream/DocustreamApplication.java
@@ -0,0 +1,13 @@
+package com.aps.docustream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocustreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocustreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aps/docustream/DocustreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.docustream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocustreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
